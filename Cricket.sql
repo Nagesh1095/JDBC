9f0703c5ef0126a064dd93bf5956c3fff9bda441
Nagesh1095 <nagesh.xworkz000@gmail.com>
2022-03-09 07:50:04 +0530

    JDBC

---
+++ Cricket.sql
@@ -0,0 +1,7 @@
+create table cricket_details(
+no_of_players smallint,
+team_name varchar(20) primary key,
+captain_name varchar(20),
+matchs_played int);
+
+select *from cricket_details;
---
+++ Mobile.sql
@@ -0,0 +1,9 @@
+create table mobile_details(
+mobile_name varchar(25) primary key,
+model varchar(20),
+ram_size smallint,
+storage_capacity int);
+
+
+select * from mobile_details;
+
